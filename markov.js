8511ee685459c0732c23a2fee5b1bc93ace4cd33
Soren Thompson <sorenct04@gmail.com>
2016-05-27 15:17:52 -0600

    updated markov-tweets

--- markov.js
+++ markov.js
@@ -1,14 +1,23 @@
 function split(s) {
-	if (s ===""){
-		return [];
-	} else {
-		var cleanArray = s.split(" ")	
-		return cleanArray.filter(function(str){
+	return s.split(" ")
+		.filter(function(str){
 			return str !== ""
-		})
-	}
+	})
 }
+
+// 1. a function that returns a count for each word in the string
+// 2. string 
+//3. none
+//4. none
+//5. An object that contains the word count.
+function count(str){
+	return split(str).reduce(function(finisher, word){
+ 		finisher[word] = finisher[word] ? finisher[word]+1 : 1
+ 		return finisher	
+ 	}, {})
+}	
 
 module.exports = {
 	split: split,
+	count: count,
 }
--- test/test.js
+++ test/test.js
@@ -18,5 +18,19 @@ describe('Markov tweets', function() {
 		it('"I have a dog.  " should produce the array ["I", "have", "a", "dog."] ',function(){
 			expect(markov.split('I have a dog.  ')).to.eql(["I", "have", "a", "dog."])
 		})
+	})
+	describe('function that returns a count for each word in the string', function() {
+		it('produces an empty object from an empty string', function(){
+			expect(markov.count("")).to.eql({})
+		})
+		it('return an object count using a one word string', function(){
+			expect(markov.count("dog")).to.eql({dog:1})
+		})
+		it('return an object count using a two word string', function(){
+			expect(markov.count("dog smelled")).to.eql({dog:1, smelled:1})
+		})
+		it('"The dog smelled the other dog" it returns {The: 1, dog: 2, smelled: 1, the: 1, other: 1}', function(){
+			expect(markov.count("The dog smelled the other dog")).to.eql({The: 1, dog: 2, smelled: 1, the: 1, other: 1})
+		})
 	})	
 })
